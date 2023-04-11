ce70d0d6e9c2c33dd3a96b12c6778d2c03a980c2
mojave2 <chenchen145@huawei.com>
2023-04-11 15:02:36 +0800

    implement issue#5
    Signed-off-by: mojave2 <chenchen145@huawei.com>

--- src/cli.rs
+++ src/cli.rs
@@ -1,1 +1,44 @@
+use std::path::PathBuf;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    /// The path to the checks configuration file, which is demanded.
+    #[arg(short, long = "rule-file", value_parser = check_file_existence)]
+    rule_file: PathBuf,
+
+    /// The path to the source code file that will be checked.
+    #[arg(short, long = "src-file", value_parser = check_file_existence)]
+    src_file: PathBuf,
+
+    /// The path to the output file.
+    #[arg(short, long, value_parser = check_dir_existence)]
+    output: PathBuf,
+}
+
+fn check_file_existence(p: &str) -> Result<PathBuf, String> {
+    let p = PathBuf::from(p);
+    if p.exists() {
+        Ok(p)
+    } else {
+        Err(format!("The file you provide doesn't exist: {p:?}"))
+    }
+}
+
+fn check_dir_existence(p: &str) -> Result<PathBuf, String> {
+    let p = PathBuf::from(p);
+    let parent = match p.parent() {
+        Some(v) => v,
+        None => return Err(format!("The file path you provid is invalid: {p:?}")),
+    };
+
+    if parent.exists() {
+        Ok(p)
+    } else {
+        Err(format!(
+            "The file's directory you provide doesn't exist: {p:?}"
+        ))
+    }
+}
