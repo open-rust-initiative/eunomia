19bbd7db1af7db67da0cfab9468f577482c5454e
J-ZhengLi <lizheng135@huawei.com>
2023-04-11 20:13:42 +0800

    use new [`RulesCfg`] format and modify test cases
    Signed-off-by: J-ZhengLi <lizheng135@huawei.com>

--- src/parser/rules_config.rs
+++ src/parser/rules_config.rs
@@ -1,129 +1,16 @@
 use super::Deserialize;
-use crate::Result;
-use anyhow::bail;
-use std::{collections::HashSet, path::Path};
+use std::collections::HashSet;
+use std::path::Path;
 
-pub trait ToolOpt<'ru> {
-    fn enabled(&self) -> bool;
-    fn compilation_opts(&self) -> Option<&'ru str>;
-}
-
-/// The configuration for checked rules.
+/// The user defined rules configuration.
+///
+/// User can specify which file to check, what compilation options to pass,
+/// and what guidelines the checks will be referenced.
 #[derive(Debug, Deserialize)]
 pub struct RulesCfg<'ru> {
     pub file_path: &'ru Path,
     #[serde(default)]
     pub supplement_compilation_options: Option<&'ru str>,
     #[serde(default)]
-    pub lints: Lints<'ru>,
-    #[serde(default)]
-    pub miri: Miri<'ru>,
-    #[serde(default)]
-    pub sanitizer: Sanitizer<'ru>,
-}
-
-#[derive(Debug, Deserialize, Default)]
-struct BasicToolOpt<'ru> {
-    enable: bool,
-    supplement_compilation_options: Option<&'ru str>,
-}
-
-/// Lint configurations, including clippy lints and rustc lints.
-#[derive(Debug, Deserialize, Default)]
-pub struct Lints<'ru> {
-    #[serde(borrow, flatten)]
-    opts: BasicToolOpt<'ru>,
-    pub clippy: LintsOpt<'ru>,
-    pub rustc: LintsOpt<'ru>,
-}
-
-/// Lints option.
-///
-/// Each group represent how the user want to handle the specified lints.
-// TODO: Add custome deserializer to prevent intersection, but right now,
-// we'll settle for calling a verifier method after deserializing is done.
-#[derive(Debug, Deserialize, Default, PartialEq)]
-pub struct LintsOpt<'ru> {
-    #[serde(borrow)]
-    pub deny: Vec<&'ru str>,
-    pub warn: Vec<&'ru str>,
-    pub allow: Vec<&'ru str>,
-}
-
-impl LintsOpt<'_> {
-    /// Verify the lints option to see if duplicate elements exist.
-    ///
-    /// For example, if user accidently wrote `clippy::all` in the deny list,
-    /// the wrote it again in the warn list. This doesn't considered as error by clippy or
-    /// rustc, but the result might differ due to which option gets passed at last.
-    /// So, this method is to prevent that situation.
-    pub fn verify(&self) -> Result<()> {
-        let mut all = Vec::new();
-        all.extend_from_slice(&self.deny);
-        all.extend_from_slice(&self.warn);
-        all.extend_from_slice(&self.allow);
-
-        let hs_all: HashSet<&str> = HashSet::from_iter(all.iter().cloned());
-
-        if all.len() != hs_all.len() {
-            bail!("Lints configuration contains duplicate elements");
-        }
-        Ok(())
-    }
-}
-
-#[derive(Debug, Deserialize, Default)]
-pub struct Miri<'ru> {
-    #[serde(borrow, flatten)]
-    opts: BasicToolOpt<'ru>,
-}
-
-/// Supporting types of sanitizer,
-/// see [sanitizer doc](https://doc.rust-lang.org/nightly/unstable-book/compiler-flags/sanitizer.html)
-/// for more information.
-#[derive(Debug, Deserialize, PartialEq)]
-pub enum SanitizerType {
-    Address,
-    Memory,
-    Leak,
-    Thread,
-}
-
-#[derive(Debug, Deserialize, Default)]
-pub struct Sanitizer<'ru> {
-    #[serde(borrow, flatten)]
-    opts: BasicToolOpt<'ru>,
-    #[serde(default = "default_sanitizer_types")]
-    /// A list of supporting sanitizers, as defined as [`SanitizerType`].
-    pub types: Vec<SanitizerType>,
-}
-
-fn default_sanitizer_types() -> Vec<SanitizerType> {
-    vec![SanitizerType::Address]
-}
-
-// TODO: these impls are redundent, use derive macros.
-impl<'ru> ToolOpt<'ru> for Lints<'ru> {
-    fn enabled(&self) -> bool {
-        self.opts.enable
-    }
-    fn compilation_opts(&self) -> Option<&'ru str> {
-        self.opts.supplement_compilation_options
-    }
-}
-impl<'ru> ToolOpt<'ru> for Miri<'ru> {
-    fn enabled(&self) -> bool {
-        self.opts.enable
-    }
-    fn compilation_opts(&self) -> Option<&'ru str> {
-        self.opts.supplement_compilation_options
-    }
-}
-impl<'ru> ToolOpt<'ru> for Sanitizer<'ru> {
-    fn enabled(&self) -> bool {
-        self.opts.enable
-    }
-    fn compilation_opts(&self) -> Option<&'ru str> {
-        self.opts.supplement_compilation_options
-    }
+    pub coding_guidelines: HashSet<&'ru str>,
 }
--- tests/parse_rules_config.rs
+++ tests/parse_rules_config.rs
@@ -8,31 +8,13 @@ fn de_normal_rules() {
 {
     "file_path": "./src/main.rs",
     "supplement_compilation_options": "-I../a/b/c/d sources=../e/f/g/h.rs",
-    "lints": {
-        "enable": true,
-        "supplement_compilation_options": "",
-        "clippy": {
-            "deny": [],
-            "warn": [
-                "all",
-                "pedantic"
-            ],
-            "allow": [ "cargo" ]
-        },
-        "rustc": {
-            "deny": [],
-            "warn": [],
-            "allow": []
-        }
-    },
-    "miri": {
-        "enable": true,
-        "supplement_compilation_options": ""
-    },
-    "sanitizer": {
-        "enable": true,
-        "supplement_compilation_options": ""
-    }
+    "coding_guidelines": [
+        "P.Exam.Ple.01",
+        "P.Exam.Ple.02",
+        "P.Exam.Ple.03",
+        "G.Exam.Ple.01",
+        "G.Exam.Ple.02"
+    ]
 }
 "#;
 
@@ -40,28 +22,22 @@ fn de_normal_rules() {
     assert!(test_cfg.is_ok());
 
     let cfg = test_cfg.unwrap();
-    let compilation_options = Some("");
     assert_eq!(cfg.file_path, Path::new("./src/main.rs"));
     assert_eq!(
         cfg.supplement_compilation_options,
         Some("-I../a/b/c/d sources=../e/f/g/h.rs")
     );
-    assert!(cfg.lints.enabled());
-    assert_eq!(cfg.lints.compilation_opts(), compilation_options);
     assert_eq!(
-        cfg.lints.clippy,
-        LintsOpt {
-            deny: Vec::new(),
-            warn: Vec::from(["all", "pedantic"]),
-            allow: Vec::from(["cargo"])
-        }
+        cfg.coding_guidelines,
+        [
+            "P.Exam.Ple.01",
+            "P.Exam.Ple.02",
+            "P.Exam.Ple.03",
+            "G.Exam.Ple.01",
+            "G.Exam.Ple.02",
+        ]
+        .into()
     );
-    assert_eq!(cfg.lints.rustc, LintsOpt::default());
-    assert!(cfg.miri.enabled());
-    assert!(cfg.sanitizer.enabled());
-    assert_eq!(cfg.miri.compilation_opts(), compilation_options);
-    assert_eq!(cfg.sanitizer.compilation_opts(), compilation_options);
-    assert_eq!(cfg.sanitizer.types, [SanitizerType::Address]);
 }
 
 #[test]
@@ -77,46 +53,16 @@ fn de_default_rules() {
     let cfg = test_cfg.unwrap();
     assert_eq!(cfg.file_path, Path::new("./src/main.rs"));
     assert!(cfg.supplement_compilation_options.is_none());
-    assert!(!cfg.lints.enabled());
-    assert!(cfg.lints.compilation_opts().is_none());
-    assert_eq!(cfg.lints.clippy, LintsOpt::default());
-    assert_eq!(cfg.lints.rustc, LintsOpt::default());
-    assert!(!cfg.miri.enabled());
-    assert!(!cfg.sanitizer.enabled());
-    assert!(cfg.miri.compilation_opts().is_none());
-    assert!(cfg.sanitizer.compilation_opts().is_none());
-    assert!(cfg.sanitizer.types.is_empty());
+    assert!(cfg.coding_guidelines.is_empty());
 }
 
 #[test]
-fn de_rules_with_duplicate_lints() {
+fn de_rules_missing_path() {
     let rule_str = r#"
     {
-        "file_path": "./src/main.rs",
-        "lints": {
-            "enable": true,
-            "clippy": {
-                "warn": [
-                    "all",
-                    "pedantic"
-                ],
-                "allow": [ "cargo" ],
-                "deny": [ "all" ]
-            },
-            "rustc": {
-                "deny": ["unused"],
-                "warn": ["all"],
-                "allow": []
-            }
-        }
-    }
-    "#;
+        "supplement_compilation_options": "-I../a/b/c/d sources=../e/f/g/h.rs"
+    }"#;
 
     let test_cfg: Result<RulesCfg, _> = serde_json::from_str(rule_str);
-    assert!(test_cfg.is_ok());
-    let cfg = test_cfg.unwrap();
-    // clippy lints option contains dup
-    assert!(cfg.lints.clippy.verify().is_err());
-    // rustc lints option does not contains dup
-    assert!(cfg.lints.rustc.verify().is_ok());
+    assert!(test_cfg.is_err());
 }
